0cc1f1c99c08b224248d1d9efc566f7170eb584d
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-05-26 08:24:47 +0800

    chore: add more JSDocs

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,75 @@
 export interface CreateGQLOptions {
+	/**
+	 * The HTTP method to use when sending the query
+	 * @default POST
+	 */
 	method?: string;
+
+	/**
+	 * The headers to add to the query request.
+	 *
+	 * For instance, you can use this to add `Authorization` headers to make authenticated requests.
+	 */
 	headers?: HeadersInit;
 }
 
+/** A successful GraphQL response (i.e. 2xx status code) */
 export interface GraphQLResponseSuccess {
+	/** Whether the request was successful */
 	success: true;
+	/** The data returned from the GraphQL API */
 	data: unknown;
 }
+
+/** A failed GraphQL response */
 export interface GraphQLResponseFailure {
+	/** Whether the request was successful */
 	success: false;
+	/** The raw Response returned from `fetch` */
 	response: Response;
 }
+
+/**
+ * A GraphQL response, either successful or not
+ *
+ * Discriminate via the `success` key.
+ */
 export type GraphQLResponse = GraphQLResponseSuccess | GraphQLResponseFailure;
 
+/**
+ * Creates a GraphQL template literal tag from a URL endpoint and an optional set of options
+ *
+ * @param url The URL endpoint of the GraphQL API
+ * @param options Options for the GraphQL instance
+ * @returns A GraphQL template literal tag
+ */
 export const createGql = (url: string, options?: CreateGQLOptions) => {
-	return (a: TemplateStringsArray, ...b: unknown[]) => {
-		const query = String.raw({ raw: a }, ...b);
+	/**
+	 * Creates a request async function from a query template literal that can be called to get a response
+	 *
+	 * @param query The GraphQL query
+	 * @param substitutions Substitutions (this is a template literal internal)
+	 * @returns An async function for making the request
+	 */
+	const h = (query: TemplateStringsArray, ...substitutions: unknown[]) => {
+		const query2 = String.raw({ raw: query }, ...substitutions);
 
-		return async (
+		/**
+		 * Makes the actual GraphQL request
+		 *
+		 * @param variables GraphQL variables to send additionally to the GraphQL API
+		 * @returns The response from the API
+		 */
+		const f = async (
 			variables?: Record<string, unknown>
 		): Promise<GraphQLResponse> => {
 			const res = await fetch(url, {
 				method: options?.method ?? "POST",
 				headers: { "Content-Type": "application/json", ...options?.headers },
-				body: JSON.stringify({ query, ...(variables ? { variables } : {}) }),
+				body: JSON.stringify({
+					query: query2,
+					...(variables ? { variables } : {}),
+				}),
 			});
 
 			if (!res.ok) {
@@ -32,5 +78,9 @@ export const createGql = (url: string, options?: CreateGQLOptions) => {
 
 			return { success: true, data: (await res.json()) as unknown };
 		};
+
+		return f;
 	};
+
+	return h;
 };
